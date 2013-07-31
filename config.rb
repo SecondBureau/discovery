1237ca2ba79e6d54de9156767742b9310ea6e88b
Gilles Crofils <gilles@secondbureau.com>
2013-08-01 00:57:27 +0200

    auto resize home for australia

--- config.rb
+++ config.rb
@@ -41,7 +41,7 @@ end
 ###
 
 # Automatic image dimensions on image_tag helper
- activate :automatic_image_sizes
+ #activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
  activate :livereload
@@ -62,10 +62,10 @@ set :images_dir, 'images'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+   activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+   activate :minify_javascript
 
   # Enable cache buster
   # activate :asset_hash
